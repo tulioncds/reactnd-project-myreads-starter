34146fd08e21a0eb61986e38665c6cc8b0f81511
Demo <demo@MacBook-Air-de-Demo.local>
2018-03-20 22:04:55 -0300

    feat:create search component with route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React, {Component} from 'react'
 import BooksList from './BooksList'
+import BookSearch from './BookSearch'
 // import * as BooksAPI from './BooksAPI'
 import { Route } from 'react-router-dom'
 import './App.css'
 
 class App extends Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
   
   render() {
     return (
       <div>
         <Route path='/' component={BooksList}/>
+        <Route path='/search' component={BookSearch} />
       </div>
     )
   }
