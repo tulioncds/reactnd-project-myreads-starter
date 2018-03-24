b720082569e0e5b912c88fb9974638fffa9714f0
Demo <demo@MacBook-Air-de-Demo.local>
2018-03-24 19:05:43 -0300

    feat: get and render all books from backend

--- src/App.js
+++ src/App.js
@@ -2,18 +2,31 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import BooksList from './BooksList';
 import BookSearch from './BookSearch';
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './utils/BooksAPI';
 import './App.css';
 
 class App extends Component {
-  componentDidMount() {
+  state = {
+    books: [],
+  }
 
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books });
+    });
   }
 
   render() {
     return (
       <div>
-        <Route path='/' component={BooksList} />
+        <Route
+          path='/'
+          render={() => (
+            <BooksList
+              books={this.state.books}
+            />
+          )}
+        />
         <Route path='/search' component={BookSearch} />
       </div>
     );
